dc4a4c681a2d6b5079de10134dc1df2f9e0ab0b1
shualton <shualton@gmail.com>
2019-06-06 02:02:16 -0700

    used ajax function to grab gif from api, then also created pause function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -20,4 +20,53 @@ $(document).ready(function(){
             $("#animalbuttons").append(button);
          }
     }
+    $("#addAnimal").on("click", function(event) {
+        event.preventDefault();
+        let topic = $("#animal-input").val().trim(); 
+        if (topic!==""){        
+            topics.push(topic);
+            renderButtons();
+
+        }
+    });
+
+    
+
+    $(document).on("click",".animalButtons", function(){
+        $("#animals").empty();
+        let animalName = $(this).val();
+        let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalName + "&api_key=FNSlJ3B5F3zJwMZdPxfukk7N8aZPgZjZ&limit=10"
+        let j, images=""
+        let x = "480w_still";
+        $.ajax({
+            url:queryURL,
+            method: "GET"
+            }).then(function(response){
+                for (j in response.data){
+                    console.log(response.data[j].images[x].url);
+                    images =`<div class="panel panel-primary col-md-4 col-sm-4 col-xs-6">
+                                <img class="staticImage col-md-12 " data-name="${j}" src="${response.data[j].images[x].url}" alt="${animalName}" width="250px" height="250px">
+                                <h3>Rating:  ${response.data[j].rating}</span></h3>
+                            </div>`
+                            console.log(animalName)
+                    $("#animals").append(images);
+                    
+                }
+                $(document).on("click",".staticImage", function(){
+                    let dataNumber = $(this).attr("data-name");
+                    $(this).attr("src",response.data[dataNumber].images.downsized.url);
+                    $(this).removeClass("staticImage");
+                    $(this).addClass("animatedImage");
+                });  
+                    $(document).on("click",".animatedImage", function(){
+                    let dataNumber = $(this).attr("data-name");
+                    $(this).attr("src",response.data[dataNumber].images[x].url); 
+                    $(this).removeClass("animatedImage");
+                    $(this).addClass("staticImage");
+                });  
+
+            });
+    });
+
+   c
 });
